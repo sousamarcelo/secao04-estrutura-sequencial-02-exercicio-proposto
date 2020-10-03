f76d53f225c9fc8a8ed252a98fd222cf2ca7e7cb
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2020-10-02 21:43:33 -0300

    Seção-04, exercicio 02

---
+++ src/Exercicio02.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+//casas decimais conforme exemplos.
+//Fórmula da área: area = π . raio2
+//Considere o valor de π = 3.14159
+
+public class Exercicio02 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite o valor de raio: ");
+		double raio = sc.nextDouble();
+		double pi = 3.14159;
+		
+		double area = pi*Math.pow(raio, 2.0);
+		
+		System.out.printf("A = %.4f%n ", area);
+		
+		sc.close();
+
+	}
+
+}
